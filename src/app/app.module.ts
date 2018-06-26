c0e70085edb8bbebc99ea4c492f1de0fc169e0b5
Daniele Agueci <d.agueci1@gmail.com>
2018-06-26 18:45:23 -0400

    Implement flights component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatRadioModule, MatSelectModule, MatSlideToggleModule, MatCardModule, MatDividerModule, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule } from '@angular/material';
+import { MatRadioModule, MatSelectModule, MatSlideToggleModule, MatCardModule, MatDividerModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatTooltipModule } from '@angular/material';
 
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
@@ -35,7 +36,8 @@ import { HomeComponent } from './home/home.component';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatMenuModule
+    MatMenuModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/flights/flights.component.ts
+++ src/app/flights/flights.component.ts
@@ -7,12 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlightsComponent implements OnInit {
 
-  selectedFilterId = 1;
   filters = [
     { id: 1, display: 'All' },
-    { id: 2, display: 'Upcoming' },
-    { id: 3, display: 'Completed' }
+    { id: 2, display: 'Upcoming', icon: 'access_time' },
+    { id: 3, display: 'Completed', icon: 'check' }
   ];
+
+  selectedFilterId = 1;
+
+  flights = [
+    { id: 1, status: 2, flightNumber: 'AC360', origin: 'Toronto', destination: 'Amsterdam', date: 'June 26, 2018' },
+    { id: 2, status: 3, flightNumber: 'BA157', origin: 'London', destination: 'Belize', date: 'June 22, 2018' },
+    { id: 2, status: 3, flightNumber: 'AC637', origin: 'Vancouver', destination: 'Hawaii', date: 'August 5, 2017' }
+  ];
+
+  filteredFlights = this.flights;
 
   constructor() { }
 
@@ -21,10 +30,19 @@ export class FlightsComponent implements OnInit {
 
   selectFilter(id) {
     this.selectedFilterId = id;
+    this.filteredFlights = this.flights.filter(flight => id === 1 ? true : flight.status === id);
   }
 
   isFilterSelected(id) {
     return this.selectedFilterId === id;
+  }
+
+  getStatusText(id) {
+    return this.filters.find(filter => filter.id === id).display;
+  }
+
+  getStatusIcon(id) {
+    return this.filters.find(filter => filter.id === id).icon;
   }
 
 }
