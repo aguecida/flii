8dc6c0897fb352aae84cd0fcfb57d1b25dc21d8b
Daniele Agueci <d.agueci1@gmail.com>
2018-06-26 23:32:25 -0400

    Add star rating system

--- src/app/ratings/ratings.component.ts
+++ src/app/ratings/ratings.component.ts
@@ -8,9 +8,51 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 })
 export class RatingsComponent implements OnInit {
 
+  selectedStar: number = null;
+
+  stars = [
+    { number: 1, icon: 'star_border', selected: false },
+    { number: 2, icon: 'star_border', selected: false },
+    { number: 3, icon: 'star_border', selected: false },
+    { number: 4, icon: 'star_border', selected: false },
+    { number: 5, icon: 'star_border', selected: false }
+  ];
+
   constructor(public dialogRef: MatDialogRef<RatingsComponent>, @Inject(MAT_DIALOG_DATA) public data) { }
 
   ngOnInit() {
+  }
+
+  hoverStar(hoveredStar) {
+    this.stars.forEach(star => {
+      if (star.number <= hoveredStar || star.selected) {
+        star.icon = 'star';
+      }
+      else {
+        star.icon = 'star_border';
+      }
+    });
+  }
+
+  unhoverStar() {
+    this.stars.forEach(star => {
+      star.icon = star.selected ? 'star' : 'star_border';
+    });
+  }
+
+  selectStar(selectedStar) {
+    this.selectedStar = selectedStar;
+
+    this.stars.forEach(star => {
+      if (star.number <= selectedStar) {
+        star.selected = true;
+        star.icon = 'star';
+      }
+      else {
+        star.selected = false;
+        star.icon = 'star_border';
+      }
+    });
   }
 
 }
